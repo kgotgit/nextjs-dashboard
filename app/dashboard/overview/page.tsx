0212037e2d077dc3ae95af8f3df9dc20f105a847
Karthik Gotrala <6868408+kgotgit@users.noreply.github.com>
2024-01-10 22:49:07 -0600

    feat: updated loading overview page

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -2,9 +2,9 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue } from '../lib/data';
-import { fetchLatestInvoices } from '../lib/data';
-import { fetchCardData } from '../lib/data';
+import { fetchRevenue } from '../../lib/data';
+import { fetchLatestInvoices } from '../../lib/data';
+import { fetchCardData } from '../../lib/data';
 
  
 export default async function Page() {
